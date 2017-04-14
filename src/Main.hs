a4c46c310662cce498aa09204dffbf1b1470b912
Travis Poulsen <travis.poulsen@gmail.com>
2017-04-13 21:29:37 -0400

    Don't overwrite a pre-existing config file

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,7 @@ module Main where
 import           ComponentGenerator
 import           Config
 import           Data.Monoid               ((<>))
-import           Data.Text                 (pack)
+import           Data.Text                 (Text, pack)
 import           Filesystem.Path.CurrentOS (encodeString, fromText, (</>))
 import           Options.Applicative       (execParser)
 import           Parser
@@ -22,13 +22,18 @@ main = do
     Init -> do
       appRoot <- projectRoot
       let configLocation = appRoot </> (fromText . filename $ configTemplate)
-      echo "Writing config file:"
-      echo $ contents configTemplate
-      writeTextFile configLocation  (contents configTemplate)
-      echo $ "Config generated at " <> (pack . encodeString $ configLocation)
+      dirExists <- testfile configLocation
+      if dirExists
+        then echo $ filePathToText configLocation <> " already exists; exiting without action."
+      else do
+        echo "Writing config file:"
+        echo $ contents configTemplate
+        writeTextFile configLocation  (contents configTemplate)
+        echo $ "Config generated at " <> filePathToText configLocation
     Generate settings -> do
       configFile <- readConfig
       generateDesiredTemplates $ mergeConfig configFile settings
   echo "Done"
 
-
+filePathToText :: OSFilePath -> Text
+filePathToText = pack . encodeString
