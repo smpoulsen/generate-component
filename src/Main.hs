ec54e3dcba99fa773b5bdc35ed69a8cd4c323a25
Travis Poulsen <travis.poulsen@gmail.com>
2017-04-13 21:21:29 -0400

    Improve messaging to user when generating a config file
    Prints config file contents and destination to the terminal.

--- src/Main.hs
+++ src/Main.hs
@@ -6,8 +6,10 @@ module Main where
 
 import           ComponentGenerator
 import           Config
-import           Options.Applicative (execParser)
-import           Filesystem.Path.CurrentOS (fromText)
+import           Data.Monoid               ((<>))
+import           Data.Text                 (pack)
+import           Filesystem.Path.CurrentOS (encodeString, fromText, (</>))
+import           Options.Applicative       (execParser)
 import           Parser
 import           Templates
 import           Turtle.Prelude
@@ -18,8 +20,12 @@ main = do
   command <- execParser opts
   case command of
     Init -> do
-      echo "Writing config file..."
-      writeTextFile (fromText . filename $ configTemplate) (contents configTemplate)
+      appRoot <- projectRoot
+      let configLocation = appRoot </> (fromText . filename $ configTemplate)
+      echo "Writing config file:"
+      echo $ contents configTemplate
+      writeTextFile configLocation  (contents configTemplate)
+      echo $ "Config generated at " <> (pack . encodeString $ configLocation)
     Generate settings -> do
       configFile <- readConfig
       generateDesiredTemplates $ mergeConfig configFile settings
